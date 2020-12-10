5a6b10cff65d0720db39e38804a414e610b1f787
marlonggt <mgarciatabora@gmail.com>
2020-12-10 17:54:26 -0600

    se agrego gitignore y carpeta imagenes para agregar cualquier imagen que vaya en el proyecto.

--- src/sample/LoginVotanteController.java
+++ src/sample/LoginVotanteController.java
@@ -1,13 +1,9 @@
 package sample;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -18,7 +14,7 @@ public class LoginVotanteController {
 
     public void initialize(URL url, ResourceBundle resourceBundle){
 
-        Image image = new Image("/Imagen/tse.png");
+        Image image = new Image("/Imagenes/tse.png");
         logoImage.setImage(image);
 
     }
