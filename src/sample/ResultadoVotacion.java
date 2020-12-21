1763b954b2d2b2551548d54d38832e3190e7133a
ArnolMartinez <73319975+ArnolMartinez@users.noreply.github.com>
2020-12-21 14:30:41 -0600

    formularios de votacion con herencia

--- src/sample/ResultadoVotacion.java
+++ src/sample/ResultadoVotacion.java
@@ -1,18 +1,31 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.Event;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
-public class ResultadoVotacion  {
+public class ResultadoVotacion implements Initializable {
+    @FXML private TableColumn<DatosColumna,String> candidato;
+    @FXML private TableColumn<DatosColumna,String>partido;
+    @FXML private TableView<DatosColumna> lista;
+    ObservableList<DatosColumna> list= FXCollections.observableArrayList();
 
     int contadorVotosPresidente;
     int ContadorVotosDiputado;
@@ -57,5 +70,43 @@ public class ResultadoVotacion  {
         return resultado;
     }
 
+    public String lugar(){
+        File archivo=new File("UbicacionVotante");
+        String linea="";
+        try {
+            Scanner entrada=new Scanner(archivo);
+            while (entrada.hasNextLine()){
+                linea=entrada.nextLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return linea;
+    }
+public void tabla(String documento){
+    String u=lugar();
+    String b[]=u.split("-");
+    File archivo=new File(documento);
+    try {
+        Scanner entrada=new Scanner(archivo);
+        while (entrada.hasNextLine()){
+            String linea=entrada.nextLine();
+            String info[]=linea.split("-");
+            if(info[2].equals(b[0])&&info[3].equals(b[1])){
+                getCandidatos.add(info[1]);
+                list.addAll(new DatosColumna(info[1],info[0]));
+            }
 
+        }
+    } catch (FileNotFoundException e) {
+        e.printStackTrace();
+    }
+    lista.setItems(list);
+    candidato.setCellValueFactory(new PropertyValueFactory<DatosColumna,String>("candidato"));
+    partido.setCellValueFactory(new PropertyValueFactory<DatosColumna,String>("partido"));
+}
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
